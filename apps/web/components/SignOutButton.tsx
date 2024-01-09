408f9e5b9142d402043e93c44f83e574c820288a
Nayan Bagale <nvbagale@gmail.com>
2024-01-10 02:23:22 +0530

    Bugfix: Signout button is not clicking.

--- apps/web/components/SignOutButton.tsx
+++ apps/web/components/SignOutButton.tsx
@@ -5,7 +5,7 @@ import { signOut } from 'next-auth/react';
 
 const SignOutButton = () => {
   return (
-    <div className='flex items-center' onClick={() => signOut({ callbackUrl: "/" })}>
+    <div className='flex items-center w-full' onClick={() => signOut({ callbackUrl: "/" })}>
       <LogOut className="mr-2 h-4 w-4" />
       Log out
     </div>
