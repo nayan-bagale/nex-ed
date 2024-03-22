256b58a9d3e0e75265a292fb057cfe87ddb01ecd
Nayan <nvbagale@gmail.com>
2024-03-23 00:59:17 +0530

    Update home page layout and font size

--- apps/web/app/(home)/page.tsx
+++ apps/web/app/(home)/page.tsx
@@ -36,9 +36,9 @@ export default async function Home() {
       </div>
 
       <section className="pt-16">
-        <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+        <div className="h-[calc(100vh-4rem)] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
           <div className="max-w-3xl mx-auto p-4">
-            <h1 className="relative z-10 text-2xl md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+            <h1 className="relative z-10 text-4xl md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
               Welcome to Nex-Ed
             </h1>
             <p></p>
@@ -53,7 +53,7 @@ export default async function Home() {
           </div>
           <BackgroundBeams className=" bg-inherit" />
         </div>
-            <GlowingStarsBackgroundCardPreview />
+            {/* <GlowingStarsBackgroundCardPreview /> */}
       </section>
     </main>
   );
